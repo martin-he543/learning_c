6bccaa1cd6412541f7b878ea75df913be0e7de6c
Martin He <martin.he543@outlook.com>
2022-07-27 19:24:48 +0100

    Changed hello_world files around

--- hello_world.c
+++ hello_world.c
@@ -1,17 +1,5 @@
 #include <stdio.h>
-#include <math.h>
 
-void main()
+int main(void)
 {
-    char input[80];
-    int x, a, b, c;
-    printf("Please input your name!\n");
-    scanf("%s", input);
-    printf("Hello %s!\n", input);
-
-    printf("Please input your age\n");
-    scanf("%d", &x);
-    printf("You are %d years old!\n", x);
-
-    printf("%.02f\n", sqrt(x));
 }
---
+++ hello_world_extended.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+    char input[80];
+    int x, a, b, c;
+    printf("Please input your name!\n");
+    scanf("%s", input);
+    printf("Hello %s!\n", input);
+
+    printf("Please input your age\n");
+    scanf("%d", &x);
+    printf("You are %d years old!\n", x);
+
+    printf("%.02f\n", sqrt(x));
+}
